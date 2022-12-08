37c83575cda4c23e3d08d5ccc399624fb73ed5ce
ev0clu <ev0clu@gmail.com>
2022-12-08 15:22:50 +0100

    Grid table is created, grid area change the color

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
 // --------- Functions declaration --------- //
 
-// Set the slider value to the default value is set at the index.html file
-function setDefaultValue(){
-  const defaultValue = document.querySelector(".slider-value");
-  defaultValue.textContent = `${slider.value} x ${slider.value}`;
 
-  const defaultContent = document.createElement("div");
-  defaultContent.setAttribute("class", "square");
-  defaultContent.setAttribute("id", `square${slider.value}`);
-  /*defaultContent.classList.add(`square${slider.value}`);*/
-  defaultContent.textContent = "1";
-
-  gridContainer.appendChild(defaultContent);
-
+function setSliderValue(value){
+  const sliderValue = document.querySelector(".slider-value");
+  sliderValue.textContent = `${value} x ${value}`;
 }
 
 // Set grid table according to the slider setting
-function setGridTable(value){
+function updateGridTable(value){
   gridContainer.textContent = "";
   const newGrid = value * value;
-  for(let i=0; i<newGrid; i++){
+  for(let i=1; i<=newGrid; i++){
     const addContent = document.createElement("div");
-    addContent.setAttribute("class", "square");
-    addContent.setAttribute("id", `square${slider.value}`);
-    gridContainer.setAttribute("style", `grid-template-columns: repeat(${value}, ${600/value}px); grid-template-rows: repeat(${value}, ${600/value}px)`);
-    addContent.classList.add(`square${++gridValue}`);
-    addContent.textContent = "5";
+    addContent.setAttribute("class", "grid-element");
+    gridContainer.setAttribute("style", `grid-template-columns: repeat(${value}, 1fr); grid-template-rows: repeat(${value}, 1fr)`);
     gridContainer.appendChild(addContent);
   }
-  
 }
 
 
+// Set initial declaration
+let isMouseDown = false;
+
 const slider = document.querySelector("#slider");
 const gridContainer = document.querySelector("#grid-container");
-let gridValue = slider.value;
-setGridTable(16);
+
+const defaultSliderValue = slider.value;
+setSliderValue(defaultSliderValue);
+updateGridTable(defaultSliderValue);
 
 
-
+// Event listener for slider
 slider.addEventListener("input", () => {
-  const sliderValue = document.querySelector(".slider-value");
-  sliderValue.textContent = `${slider.value} x ${slider.value}`;
-  
-  setGridTable(slider.value);
+  setSliderValue(slider.value);
+  updateGridTable(slider.value);
 });
+
+
+// Event listener for mouse down, mouse move and mouse up
+gridContainer.addEventListener("mousedown", (event) => {
+  // highlight the mouseover target
+  event.target.style.background = "orange";
+  isMouseDown = true;
+});
+
+
+gridContainer.addEventListener('mousemove', (event) => {
+  if (isMouseDown) {
+    event.target.style.background = "orange";
+  }
+});
+
+gridContainer.addEventListener('mouseup', (event) => {
+    isMouseDown = false;
+});
+
 
 
 
